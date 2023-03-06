a173c7ee4cafee7de4eaded78287ee81e0d7d801
ryansong612 <ryansong612@gmail.com>
2023-03-06 14:31:42 +0000

    all files added

---
+++ matrixmultiplicationlab/src/main/uk/ac/imperial/matrixmult/MatrixBuilder.java
@@ -0,0 +1,15 @@
+package uk.ac.imperial.matrixmult;
+
+
+public class MatrixBuilder {
+
+  public static Matrix build(double[][] source) {
+    // TODO
+    return null;
+  }
+
+  public static Matrix build(int nRows, int nCols) {
+    // TODO
+    return null;
+  }
+}
---
+++ matrixmultiplicationlab/src/main/uk/ac/imperial/matrixmult/MatrixMultiplier.java
@@ -0,0 +1,9 @@
+package uk.ac.imperial.matrixmult;
+
+public class MatrixMultiplier {
+
+  public static Matrix multiply(Matrix a, Matrix b) throws Exception {
+    // TODO
+    return null;
+  }
+}
